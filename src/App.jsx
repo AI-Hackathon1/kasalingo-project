2860b7ec11593b0e58bcfe5181141149ac2c8a6b
MichaelAfedi <dmus0636@gmail.com>
2025-05-19 22:08:46 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline bg-red-500">App is good </h1>
+    </>
+  )
+}
+
+export default App
